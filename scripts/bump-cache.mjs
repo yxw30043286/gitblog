b129b3eb336180707a2efd8cec5eea99e676e0e9
jimmycppliu <jimmycppliu@tencent.com>
2026-05-12 17:44:19 +0800

    ui

--- scripts/bump-cache.mjs
+++ scripts/bump-cache.mjs
@@ -2,7 +2,7 @@
 import { readFileSync, writeFileSync, readdirSync, existsSync } from 'node:fs';
 import { join, sep } from 'node:path';
 
-const NEW = '20260512172500';
+const NEW = '20260512174000';
 
 let cfg = readFileSync('assets/js/config.js', 'utf8');
 cfg = cfg.replace(/(VERSION\s*=\s*')(\d+)(')/, `$1${NEW}$3`);
